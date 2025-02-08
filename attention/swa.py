29e7aac42fd2482d5c3c067aeb7497d74896a64d
odunola <jenrola2292@gmail.com>
2025-02-08 16:50:45 +0100

    Add MLA, Positional Encoding,

--- attention/swa.py
+++ attention/swa.py
@@ -4,4 +4,10 @@ from torch import Tensor
 from typing import Optional, Dict
 
 class SlidingWindowAttention(nn.Module):
-    pass
+    def __init__(self, config):
+        super().__init__()
+        self.num_heads = config.num_heads
+        self.dim_model = config.dim_model
+        self.dim_k = config.dim_k
+        self.dim_v = config.dim_v
+        self.window_size = config.window_size
