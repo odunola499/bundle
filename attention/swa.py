ca12c8f07dc711e40068e46ee76cd9c60ef39d7d
odunola <jenrola2292@gmail.com>
2025-02-08 11:12:42 +0100

    Add MLA

---
+++ attention/swa.py
@@ -0,0 +1,7 @@
+import torch
+from torch import nn
+from torch import Tensor
+from typing import Optional, Dict
+
+class SlidingWindowAttention(nn.Module):
+    pass
---
+++ encoding/__init__.py
@@ -0,0 +1,6 @@
+import torch
+from torch import nn
+
+
+class PositionalEncoding(nn.Module):
+    pass
