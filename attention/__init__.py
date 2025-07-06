bug: remove old causal arguments

attention/__init__.py
@@ -3,6 +3,21 @@ from .attention import Attention, MultiHeadAttention, MultiQueryAttention, Multi
 from torch import nn
 from torch import Tensor
 from typing import Optional, Dict
+from dataclasses import dataclass
+
+@dataclass
+class AttentionConfig:
+    dim_ff: int
+    dim_k: int
+    dim_v: int
+    num_heads: int
+    dim_model:int
+    latent_dim:int
+    is_causal:bool
+    num_group_heads:int
+
+    dropout: float = 0.1
+
 
 class DecoderAttention(nn.Module):
     def __init__(self, config):
@@ -32,11 +47,11 @@ class EncoderAttention(nn.Module):
             raise ValueError(f"Invalid attention type: {name}")
         attention = None
         if name == 'MHA':
-            attention = MultiHeadAttention(config, causal = False)
+            attention = MultiHeadAttention(config)
         elif name == 'MQA':
-            attention = MultiQueryAttention(config, causal = False)
+            attention = MultiQueryAttention(config)
         elif name == 'GQA':
-            attention = GroupedQueryAttention(config, causal = False)
+            attention = GroupedQueryAttention(config)
         self.attention = attention
 
     def forward(self, x:Tensor, mask:Tensor = None):

attention/attention.py
@@ -1,10 +1,11 @@
 import torch
 from torch import nn, Tensor
 from typing import Optional, Dict
+from attention import AttentionConfig
 
 
 class Attention(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config:AttentionConfig):
         super().__init__()
         self.num_heads: int = config.num_heads
         self.dim_model: int = config.dim_model

feedforward/feedforward.py
@@ -1,7 +1,8 @@
 from torch import nn
+from feedforward import FeedForwardConfig
 
 class FeedForward(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config:FeedForwardConfig):
         super().__init__()
         self.dim_model = config.dim_model
         self.dim_ff = config.dim_ff